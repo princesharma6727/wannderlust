e0294dd79b1054f742d3663e9f43ee5c61ce5ac4
princesharma6727 <princesharma.sharma6727@gmail.com>
2025-04-10 04:30:58 +0530

    Initial commit

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ if(process.env.NODE_ENV!="production"){
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const MongoStore = require('connect-mongo');
+
 
 const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
 const session=require("express-session");
+
 const flash=require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -26,7 +29,8 @@ const userRouter = require("./routes/user.js");
 
 
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
+
 
 
 main()
@@ -38,7 +42,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 app.set("view engine","ejs");
@@ -49,8 +53,21 @@ app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
 
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
+});
+store.on("error", (err) => {
+  console.log("ERROR in MONGO SESSION STORE", err);
+});
+
+
 const sessionOptions = {
-  secret: "mysupersecretcode",
+  store,
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -59,9 +76,9 @@ const sessionOptions = {
     httpOnly: true // Prevents client-side access to cookies
   },
 };
-app.get("/", (req, res) => {
-  res.send("Hi, I am root");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hi, I am root");
+// });
 app.use(session(sessionOptions));
 app.use(flash());
 
