bd1627185d842d9fe78adb34c1f458788a834e29
princesharma6727 <princesharma.sharma6727@gmail.com>
2025-04-10 12:53:18 +0530

    Fix wrapAsync import casing across all routes

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ const sessionOptions = {
   },
 };
 app.get("/", (req, res) => {
-  res.send("Hi, I am root");
+  res.redirect("/listings");
 });
 app.use(session(sessionOptions));
 app.use(flash());
