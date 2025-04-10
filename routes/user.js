63b4e964227c27c2e26702abfa5dc1ef507fbfc9
princesharma6727 <princesharma.sharma6727@gmail.com>
2025-04-10 12:19:37 +0530

    Fix wrapAsync import casing across all routes

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express"); 
 const router = express.Router();
 const User = require("../models/user.js");
-const wrapAsync = require("../utils/wrapasync.js");
+const wrapAsync = require("../utils/wrapAsync.js");
 const passport=require("passport");
 const{saveRedirectUrl}=require("../middleware.js");
 const userController=require("../controllers/user.js")
